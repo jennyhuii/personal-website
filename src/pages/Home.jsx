969d1e6f8adb0c0ef5651286287e9145779d580c
Jenny <j.hui.0130@gmail.com>
2022-07-23 20:27:29 -0400

    edit bio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,9 +20,6 @@ const Home = () => {
                 <AlignCenterContainer><StyledImage src={Headshot} height={'20rem'} /></AlignCenterContainer>
                 <StyledText>
                     Based in New York City, I am a front-end engineer currently working for a startup.
-                    I am also a painter who loves painting landscapes and plants.
-                    My work includes paintings and web apps built in javascript.
-                    During my free time, I enjoy practicing yoga and going on long walks.
                 </StyledText>
             </StyledContentContainer>
         </StyledMainContainer>
